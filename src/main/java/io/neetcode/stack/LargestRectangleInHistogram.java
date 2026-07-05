99373190cea63dae5d1d3eef5eab6feae32d4302
Vasanth S <Vasanth_Srinivasan@comcast.com>
2026-07-05 12:33:14 +0530

    added/changed readme/java files

--- src/main/java/io/neetcode/stack/LargestRectangleInHistogram.java
+++ src/main/java/io/neetcode/stack/LargestRectangleInHistogram.java
@@ -3,6 +3,26 @@ package io.neetcode.stack;
 import java.util.Stack;
 
 public class LargestRectangleInHistogram {
+
+    public int largestRectangleArea(int[] heights) {
+        int[] stack = new int[heights.length];
+        int top = 0;
+        int ma = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            int ch = (i == heights.length) ? 0 : heights[i];
+            while (top > 0 && ch < heights[stack[top - 1]]) {
+                int index = stack[--top];
+                int h = heights[index];
+                int w = (top == 0) ? i : i - stack[top - 1] - 1;
+                int a = h * w;
+                ma = Math.max(ma, a);
+            }
+            if (i < stack.length)
+                stack[top++] = i;
+        }
+        return ma;
+    }
+
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> s = new Stack<>();
         int ma = 0;
--- src/main/java/io/neetcode/stack/MaximumFrequencyStack.java
+++ src/main/java/io/neetcode/stack/MaximumFrequencyStack.java
@@ -4,8 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public class MaximumFrequencyStack {
+class FreqStack {
 
+    private Map<Integer, Stack<Integer>> stack;
+    private Map<Integer, Integer> freq;
+    private int maxfreq;
+
+    public FreqStack() {
+        stack = new HashMap<>();
+        freq = new HashMap<>();
+        maxfreq = 0;
+    }
+
+    public void push(int val) {
+        freq.put(val, freq.getOrDefault(val, 0) + 1);
+        maxfreq = Math.max(maxfreq, freq.get(val));
+        stack.putIfAbsent(freq.get(val), new Stack<>());
+        stack.get(freq.get(val)).push(val);
+    }
+
+    public int pop() {
+        int ans = stack.get(maxfreq).pop();
+        freq.put(ans, freq.get(ans) - 1);
+        if (stack.get(maxfreq).isEmpty())
+            maxfreq--;
+        return ans;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
+
+public class MaximumFrequencyStack {
 
     private Map<Integer, Stack<Integer>> ms;
     private Map<Integer, Integer> map;
@@ -30,7 +64,8 @@ public class MaximumFrequencyStack {
     public int pop() {
         int f = ms.get(freq).pop();
         map.put(f, map.get(f) - 1);
-        if (ms.get(freq).isEmpty()) freq--;
+        if (ms.get(freq).isEmpty())
+            freq--;
         return f;
     }
 }
@@ -41,4 +76,3 @@ public class MaximumFrequencyStack {
  * obj.push(val);
  * int param_2 = obj.pop();
  */
-
